8fec0fbcd1a4804c280c2d94902285171f88ff16
Alan Shirk <shirka@my.lanecc.edu>
2021-07-19 11:39:04 -0700

    changed deprecated methods

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //npm run devStart
 if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').load()
+    require('dotenv').config()
 }
 const express = require('express')
 const app = express()
@@ -18,7 +18,7 @@ app.use(express.static('public'))
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
