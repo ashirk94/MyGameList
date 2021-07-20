7bd9caf7eab81ba23faa29562865390f495bb34d
Alan Shirk <shirka@my.lanecc.edu>
2021-07-20 00:50:13 -0700

    set up author form

---
+++ routes/authors.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const Author = require('../models/author')
+const router = express.Router()
+
+//all authors
+router.get('/', (req, res) => {
+    res.render('authors/index')
+})
+
+//new author
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() })
+})
+
+//create author route
+router.post('/', (req, res) => {
+    const author = new Author({
+        name: req.body.name
+})
+    author.save(err, newAuthor => {
+        if (err) {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Error creating author'
+            })
+        }
+        else{
+            //res.redirect(`authors/${newAuthor.id}`)
+            res.redirect('authors')
+        }
+    })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
-const indexRouter = require('./routes/index')
 
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 
 app.set('view engine', 'ejs')
@@ -15,6 +16,8 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({ limit: '10mb', extended: false}))
+
 
 const mongoose = require('mongoose')
 
@@ -24,7 +27,7 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
-
+app.use('/authors', authorRouter)
 
 
 app.listen(process.env.PORT || 3000)
