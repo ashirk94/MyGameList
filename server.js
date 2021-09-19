572b8247d67657ef9b1c923cb0a17aa50e49dd77
Alan Shirk <shirka@my.lanecc.edu>
2021-09-18 21:18:48 -0700

    removed donation page, adding to next project

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 //app starts here
+
+//dependencies
 require('dotenv').config()
 
 const express = require('express')
@@ -6,12 +8,12 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const methodOverride = require('method-override')
 
+//router modules
 const indexRouter = require('./routes/index')
 const consoleRouter = require('./routes/consoles')
 const gameRouter = require('./routes/games')
-const donateRouter = require('./routes/donate')
 
-
+//express and ejs
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
@@ -21,25 +23,20 @@ app.use(express.urlencoded({ limit: '10mb', extended: false}))
 app.use(methodOverride('_method'))
 app.use(express.json())
 
+//mongo database
 const mongoose = require('mongoose')
-
 
 mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => {})
 
-/*app.use(cors({
-    origin: `${process.env.SERVER_URL}`
-}))*/
-
+//routers
 app.use('/', indexRouter)
 app.use('/consoles', consoleRouter)
 app.use('/games', gameRouter)
-app.use('/donate', donateRouter)
 
-
-
+//port
 const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log("App Running")
