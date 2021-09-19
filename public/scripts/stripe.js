f8c17a8ac7bbac7204120b17b2549d5b5c08c19e
Alan Shirk <shirka@my.lanecc.edu>
2021-09-18 20:43:33 -0700

    stripe

--- public/scripts/stripe.js
+++ public/scripts/stripe.js
@@ -1,10 +1,11 @@
+var stripe = Stripe('pk_live_51JbCZFBNmTxXVxUhShKn9ICsEY1m6CkbBgFapDOFrpBh7RWn3hI7IFxf3FfxrO3Kj28qG8SOtE1oKR3LPPOokcaZ00rA4PVi6l')
+
 const button = document.querySelector("#donate-stripe")
 button.addEventListener('click', () => {
     fetch('/donate/create-checkout-session', {
         method: 'POST',
         headers: {
-            'Authorization': 'Bearer pk_live_51JbCZFBNmTxXVxUhShKn9ICsEY1m6CkbBgFapDOFrpBh7RWn3hI7IFxf3FfxrO3Kj28qG8SOtE1oKR3LPPOokcaZ00rA4PVi6l',
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({
             items: [
